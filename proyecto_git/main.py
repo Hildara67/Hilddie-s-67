22ae34ee4187ee1e474a4a5a714cfb3a4b487a81
Hildara Hernandez <hanijaeunnie@gmail.com>
2026-05-04 22:47:13 -0600

    cambios

--- proyecto_git/main.py
+++ proyecto_git/main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from funcion_n import suma, resta, multiplicacion, division
 
 def ejecutar():
@@ -19,5 +21,18 @@ def ejecutar():
     resul_division = division(10, 2)
     print(f"Resultado de la division (10/2): {resul_division}")
 
+
+    plt.figure() 
+    plt.bar(['Suma', 'Resta'], [resul_suma, resul_resta], color='blue')
+    plt.title("Resultados: Suma y Resta")
+    plt.savefig("grafica_operaciones_1.eps") 
+    print("Archivo .eps generado (Suma y Resta).")
+
+    plt.figure() 
+    plt.bar(['Multiplicación', 'División'], [resul_mult, resul_division], color='green')
+    plt.title("Resultados: Multiplicación y División")
+    plt.savefig("grafica_operaciones_2.eps")
+    print("Archivo .eps generado (Multiplicación y División).")
+
 if __name__ == "__main__":
-    ejecutar_pruebas()
+    ejecutar()
